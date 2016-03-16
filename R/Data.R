a076ad40873c882604156d1dcffaba54ea337e5e
Chris Hamm <chamm@ku.edu>
2016-03-15 23:27:58 -0500

    Added metadata to Data.R file, updated description for LazyData: true

---
+++ R/Data.R
@@ -0,0 +1,14 @@
+#' Hostplant associations for genera of Nymphalidae
+#'
+#' Dataset from Hamm and Fordyce 2015 (Evolution) used in 
+#' Fordyce et al. 2016 (Ecology) containing nearly 1,000 records. 
+#'
+#' @format A data frame with 989 rown and 2 variable:
+#' \describe{
+#'  \item{Butterfly}{Genera, of Nymphalidae butterflies}
+#'  \item{Host}{Host, plant family}
+#'  ...
+#' }
+#'
+#' @source \url{http://datadryad.org/resource/doi:10.5061/dryad.p5v8g}
+"Nymphalidae_list"
